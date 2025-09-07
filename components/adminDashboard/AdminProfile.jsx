71955186b4d484c14a25beefae18207da5f67692
tanzia mourin <tanziamourin68@gmail.com>
2025-09-07 20:04:17 +0600

    add admin folder and page from mourin fork

---
+++ components/adminDashboard/AdminProfile.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const AdminProfile = () => {
+    return (
+        <div>
+            <h1 className='text-4xl font-bold'>This is admin profile </h1>
+        </div>
+    );
+};
+
+export default AdminProfile;
