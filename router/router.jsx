4d82204cfac50371c55ffd78b013233bc539dff7
JerinXplore <jerinjerin101325@gmail.com>
2025-09-07 17:34:19 +0600

    Created Navbar

--- router/router.jsx
+++ router/router.jsx
@@ -1,26 +1,22 @@
+// src/routes/router.jsx
 import { createBrowserRouter } from "react-router";
-import { RouterProvider } from "react-router/dom";
+
+// Layouts
+
+import DashboardLayout from "../layout/DashboardLayout";
+
 import BasicLayout from "../src/layout/BasicLayout";
+import Home from "../src/pages/Home";
+import Browse from "../src/components/Browse";
 
 export const router = createBrowserRouter([
   {
     path: "/",
-    Component: BasicLayout,
+    element: <BasicLayout />,
     children: [
-      {
-        index: true,
-        Component: Home,
-      },
+      { index: true, element: <Home /> },       
+      { path: "browse", element: <Browse /> }, 
     ],
   },
-  {
-    path: "/dashboard",
-    element: <DashboardLayout />,
-    children: [
-      {
-        index: true,
-        element: <DashboardHome />,
-      },
-    ],
-  },
+  
 ]);
