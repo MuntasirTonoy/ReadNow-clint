a474c4180c089be3be201965ae030c62796367b3
nipaayasha05 <nipaayasha05@gmail.com>
2025-09-07 14:51:35 +0600

    add sidebar

--- router/router.jsx
+++ router/router.jsx
@@ -2,16 +2,28 @@ import { createBrowserRouter } from "react-router";
 import { RouterProvider } from "react-router/dom";
 import BasicLayout from "../layout/BasicLayout";
 import Home from "../pages/Home/Home";
+import DashboardLayout from "../layout/DashboardLayout";
+import DashboardHome from "../components/dashboard/DashboardHome";
 
 export const router = createBrowserRouter([
   {
     path: "/",
     Component: BasicLayout,
-    children:[
-        {
-          index: true,
-          Component:Home
-        },
-    ]
+    children: [
+      {
+        index: true,
+        Component: Home,
+      },
+    ],
+  },
+  {
+    path: "/dashboard",
+    element: <DashboardLayout />,
+    children: [
+      {
+        index: true,
+        element: <DashboardHome />,
+      },
+    ],
   },
 ]);
