3531bb519eadba8aedd0ffa80dbecf25dec9c71b
muntasir.tonoy <tonoy947@gmail.com>
2025-09-09 16:29:49 +0600

    tonoy

--- components/adminDashboard/AdminProfile.jsx
+++ components/adminDashboard/AdminProfile.jsx
@@ -1,11 +1,12 @@
-import React from 'react';
+import React from "react";
 
 const AdminProfile = () => {
-    return (
-        <div>
-            <h1 className='text-4xl font-bold'>This is admin profile </h1>
-        </div>
-    );
+  return (
+    <div>
+      <h1 className="text-4xl font-bold">This is admin profile </h1>
+      <h2>tonoy works here</h2>
+    </div>
+  );
 };
 
 export default AdminProfile;
--- router/router.jsx
+++ router/router.jsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router";
-
+import DashboardLayout from "../layout/DashboardLayout";
 import BasicLayout from "../src/layout/BasicLayout";
 import Home from "../src/pages/Home";
 import Browse from "../src/components/Browse";
